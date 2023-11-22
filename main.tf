7a5e113d4f39ed48ee70b648ee3340b39ee6587e
admin <admin@gmail.com>
2023-11-21 20:24:47 -0600

    update

--- main.tf
+++ main.tf
@@ -181,7 +181,25 @@
 #    Name = "Ansible-slave-terra"
 #  }
 #}
+terraform {
 
+  required_providers {
+
+    aws = {
+
+      source  = "hashicorp/aws"
+
+      version = "~> 4.16"
+
+    }
+
+  }
+
+
+
+  required_version = ">= 1.2.0"
+
+}
 provider "aws" {
   region = "us-east-1"
 }
